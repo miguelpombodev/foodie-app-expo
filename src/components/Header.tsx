e83f4a1ac75c083242f26db22a231bcfa822e851
Miguel Pombo <miguel.pomboshape@modec.com>
2024-08-15 12:03:07 -0300

    fix: restyling header sections and creating state for sections variaty

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
-import { Center, Text, ScrollView, View, VStack, Icon, HStack } from "native-base";
+import { Text, ScrollView, View, VStack, Icon, HStack } from "native-base";
 import { AntDesign } from '@expo/vector-icons'
+import { useState } from "react";
 
 
 export function Header () {
+  const [sections, _] = useState([
+    "Restaurantes",
+    "Mercados",
+    "Farmácias",
+    "Bebidas",
+    "Pet Shop",
+  ])
   return (
     <VStack pl={2} pt={10} pb={3} borderBottomWidth={1} borderBottomColor="gray.100" mb={3}>
       <HStack pb={5} justifyContent="center" alignItems="center">
@@ -12,18 +20,13 @@ export function Header () {
       
       <View>
         <ScrollView horizontal showsHorizontalScrollIndicator={false} overScrollMode="never">
-          <View rounded="xl" bgColor="green.100" h={10} width={150} mr={5} alignItems="center" justifyContent="center">
-            <Text>Restaurantes</Text>
+        {
+          sections.map(section => (
+          <View rounded="xl" bgColor="gray.100" h={10} width={150} mr={5} alignItems="center" justifyContent="center">
+            <Text>{section}</Text>
           </View>
-          <View rounded="xl" bgColor="green.100" h={10} width={150} mr={5} alignItems="center" justifyContent="center">
-            <Text>Restaurantes</Text>
-          </View>
-          <View rounded="xl" bgColor="green.100" h={10} width={150} mr={5} alignItems="center" justifyContent="center">
-            <Text>Restaurantes</Text>
-          </View>
-          <View rounded="xl" bgColor="green.100" h={10} width={150} mr={5} alignItems="center" justifyContent="center">
-            <Text>Restaurantes</Text>
-          </View>
+          ))
+        }
         </ScrollView>
       </View>
     </VStack>
